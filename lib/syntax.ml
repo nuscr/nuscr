42165ff1b9e09166e007aaeb8b7325a7c96b9439
Fangyi Zhou <me@fangyi.io>
2019-09-07 13:03:17 +0100

    Print AST on success

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -1,6 +1,19 @@
 
+let render_pos pos =
+  Printf.sprintf "%d:%d"
+    pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1)
+
+let render_pos_interval (startp, endp) : string =
+  Printf.sprintf "%s to %s in: %s"
+    (render_pos startp)
+    (render_pos endp)
+    (startp.Lexing.pos_fname)
+
 type 'a located =
-  { loc: Lexing.position * Lexing.position ; value: 'a }
+  { loc: Lexing.position * Lexing.position ;
+    [@printer fun fmt interval -> fprintf fmt "%s" (render_pos_interval interval)]
+    value: 'a }
+  [@@deriving show]
 
 (* type ast =
  *   raw_ast located
@@ -10,18 +23,24 @@ type 'a located =
 
 (* a simple name *)
 type name = string
+[@@deriving show]
 
 (* a qualified name *)
 type raw_qname = name list
+[@@deriving show]
 type qname = raw_qname located
+[@@deriving show]
 
 let qname_to_string qn =
   String.concat "." qn
 
 type annotation = string
+[@@deriving show]
 
 type raw_mod_decl = { module_name: qname }
+[@@deriving show]
 type mod_decl = raw_mod_decl located
+[@@deriving show]
 
 type raw_type_decl =
   { domain: string (* where does the type come from java|xsd|... *)
@@ -30,19 +49,24 @@ type raw_type_decl =
   ; type_name: string (* the name of the defined type *)
   ; is_type: bool (* true for types, false for signatures *)
   }
+[@@deriving show]
 type type_decl = raw_type_decl located
+[@@deriving show]
 
 type payloadt =
   | PayloadName of name
   | PayloadDel of name * name (* protocol @ role *)
   | PayloadQName of qname
   | PayloadBnd of name * qname (* var : type *)
+[@@deriving show]
 
 type message = Message of { name:  name; payload: payloadt list }
              | MessageName of name
              | MessageQName of qname
+[@@deriving show]
 
 type global_interaction = raw_global_interaction located
+[@@deriving show]
 and raw_global_interaction =
   MessageTransfer of
     { message : message
@@ -60,8 +84,10 @@ and raw_global_interaction =
   (* message, from, to *)
   | Connect of message option * name * name * annotation option
   | Disconnect of name * name
+[@@deriving show]
 
 type protocol_mods = Aux | AuxExplicit | Explicit
+[@@deriving show]
 
 type raw_global_protocol =
   { name: name
@@ -74,11 +100,14 @@ type raw_global_protocol =
   ; interactions: global_interaction list
   ; ann : annotation option
   }
+  [@@deriving show]
 
 type global_protocol = raw_global_protocol located
+[@@deriving show]
 
 type scr_module =
   { decl: mod_decl
   ; types: type_decl list
   ; protocols : global_protocol list
   }
+[@@deriving show]
