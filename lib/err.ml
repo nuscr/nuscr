7c40a3d49523cf202ffc96520b0e4be781f11abb
Fangyi Zhou <me@fangyi.io>
2019-09-13 11:33:32 +0100

    Slightly refine the errors

--- lib/err.ml
+++ lib/err.ml
@@ -1,5 +1,17 @@
-exception UserError of string
+type user_error =
+  | LexerError of string
+  | ParserError of (Lexing.position * Lexing.position)
+  | Uncategorised of string
+
+exception UserError of user_error
 (** UserError is a user error and should be reported back so it can be fixed *)
+
+let show_user_error = function
+  | LexerError msg -> "Lexer error: " ^ msg
+  | ParserError interval ->
+      "Parser error: An error occurred at "
+      ^ Syntax.render_pos_interval interval
+  | Uncategorised msg -> "Error " ^ msg
 
 exception Violation of string
 (** A Violation is reported when an impossible state was reached. It has to
