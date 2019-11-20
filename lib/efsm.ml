b89b26376822d4b01144386ac6ca56753cd11397
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2019-11-20 15:02:07 +0000

    Make EFSM deterministic

--- lib/efsm.ml
+++ lib/efsm.ml
@@ -18,7 +18,7 @@ module Label = struct
   let default = Epsilon
 end
 
-module G = Imperative.Digraph.ConcreteLabeled (Int) (Label)
+module G = Persistent.Digraph.ConcreteLabeled (Int) (Label)
 
 module Display = struct
   include G
@@ -49,6 +49,30 @@ let is_internal_choice = function
       | _ -> failwith "Impossible" )
   | _ -> failwith "Impossible"
 
+type conv_env = {g: G.t; tyvars: (string * int) list}
+
+let init_conv_env = {g= G.empty; tyvars= []}
+
+(* Redirect all edges into st_remove to st_base, then remove st_remove *)
+let merge_state g st_base st_remove =
+  let f (src, action, _dst) g_ =
+    let g_ = G.add_edge_e g_ (src, action, st_base) in
+    g_
+  in
+  let g = G.fold_pred_e f g st_remove g in
+  let g = G.remove_vertex g st_remove in
+  g
+
+(* Redirect all edges from st_remove to st_base, then remove st_remove *)
+let merge_state_rev st_base g st_remove =
+  let f (_src, action, dst) g_ =
+    let g_ = G.add_edge_e g_ (st_base, action, dst) in
+    g_
+  in
+  let g = G.fold_succ_e f g st_remove g in
+  let g = G.remove_vertex g st_remove in
+  g
+
 let conv_ltype lty =
   let count = ref 0 in
   let fresh () =
@@ -56,34 +80,50 @@ let conv_ltype lty =
     count := n + 1 ;
     n
   in
-  let g = G.create () in
-  let rec conv_ltype_aux tyvars = function
+  let rec conv_ltype_aux env =
+    let {g; tyvars} = env in
+    function
     | RecvL (m, n, l) ->
         let curr = fresh () in
-        let next = conv_ltype_aux tyvars l in
+        let env, next = conv_ltype_aux env l in
+        let g = env.g in
+        let g = G.add_vertex g curr in
         let e = (curr, RecvA (n, m), next) in
-        G.add_edge_e g e ; curr
+        let g = G.add_edge_e g e in
+        ({env with g}, curr)
     | SendL (m, n, l) ->
         let curr = fresh () in
-        let next = conv_ltype_aux tyvars l in
+        let env, next = conv_ltype_aux env l in
         let e = (curr, SendA (n, m), next) in
-        G.add_edge_e g e ; curr
+        let g = env.g in
+        let g = G.add_vertex g curr in
+        let g = G.add_edge_e g e in
+        ({env with g}, curr)
     | ChoiceL (_r, ltys) ->
         let curr = fresh () in
-        let nexts = List.map ~f:(conv_ltype_aux tyvars) ltys in
+        let env, nexts = List.fold_map ~f:conv_ltype_aux ~init:env ltys in
+        let g = env.g in
         let es = List.map ~f:(fun n -> (curr, Epsilon, n)) nexts in
-        List.iter ~f:(G.add_edge_e g) es ;
-        curr
-    | EndL -> fresh ()
+        let g = G.add_vertex g curr in
+        let g = List.fold ~f:G.add_edge_e ~init:g es in
+        let g = List.fold ~f:(merge_state_rev curr) ~init:g nexts in
+        ({env with g}, curr)
+    | EndL ->
+        let curr = fresh () in
+        let g = G.add_vertex g curr in
+        ({env with g}, curr)
     | MuL (tv, l) ->
-        let n = fresh () in
-        let start = conv_ltype_aux ((tv, n) :: tyvars) l in
-        let e = (n, Epsilon, start) in
-        G.add_edge_e g e ; start
-    | TVarL tv -> List.Assoc.find_exn ~equal:String.equal tyvars tv
+        let new_st = fresh () in
+        let g = G.add_vertex g new_st in
+        let old_env = env in
+        let env = {tyvars= (tv, new_st) :: tyvars; g} in
+        let env, curr = conv_ltype_aux env l in
+        let g = merge_state env.g curr new_st in
+        ({old_env with g}, curr)
+    | TVarL tv -> (env, List.Assoc.find_exn ~equal:String.equal env.tyvars tv)
   in
-  let start = conv_ltype_aux [] lty in
-  (start, g)
+  let env, start = conv_ltype_aux init_conv_env lty in
+  (start, env.g)
 
 let show_efsm g =
   let buffer = Buffer.create 4196 in
