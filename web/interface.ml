98361afcdd7044abc990788f416355417ed3936f
Simon Castellan <simon.castellan@phis.me>
2019-11-22 09:08:05 +0000

    Split of the code of the web interface.

---
+++ web/interface.ml
@@ -0,0 +1,25 @@
+open Js_of_ocaml
+open Nuscrlib
+module Code = struct
+  let id = "protocol-textarea"
+  let get () =
+    Js.(to_string (Webutils.get_textarea id) ##. value)
+  let set s =
+    (Webutils.get_textarea id) ##. value := Js.string s
+end
+
+module Error = struct
+  let errorbox = "errorbox" 
+  let display fmt =
+    let display string =
+      let e = Webutils.get errorbox in
+      Webutils.(set_inner_html e "%s" string;
+                set_display e "block")
+    in
+    Printf.ksprintf display fmt
+  let display_exn = function
+    | Err.UserError e -> display "<b>User error</b>: %s" (Err.show_user_error e)
+    | _ -> ()
+  let reset () =
+    Webutils.(set_display (get "errorbox") "none")
+end
