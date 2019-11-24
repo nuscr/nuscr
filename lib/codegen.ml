fb73a2e7fd61bb1a2273b30de221396606b94e60
Fangyi Zhou <me@fangyi.io>
2019-11-24 15:34:27 +0000

    Use metaquot

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -1,21 +1,23 @@
 open! Core_kernel
+open! Migrate_parsetree
 
 (* open Efsm *)
 
-open Ocaml_common
+open! Ast_407
+open Parsetree
 
-let gen_ast (_proto, _role) (_start, _g) : Parsetree.structure =
-  let open Ast_helper in
-  let open Location in
-  let vb =
-    Vb.mk (Pat.var (mknoloc "hello")) (Exp.constant (Const.string "Hello"))
-  in
-  [Str.value Asttypes.Nonrecursive [vb]]
+let gen_ast (_proto, _role) (_start, _g) : structure =
+  let loc = Location.none in
+  let vb = [%stri let hello = "hello"] in
+  [vb]
+
+let migrate = Versions.migrate Versions.ocaml_407 Versions.ocaml_current
 
 let gen_code (proto, role) (start, g) =
   let buffer = Buffer.create 4196 in
   let formatter = Format.formatter_of_buffer buffer in
   let ast = gen_ast (proto, role) (start, g) in
+  let ast = migrate.copy_structure ast in
   Pprintast.structure formatter ast ;
   Format.pp_print_flush formatter () ;
   Buffer.contents buffer
