b068e47cb55f9c284d666a29f420ebfc6e0d36e3
Fangyi Zhou <me@fangyi.io>
2019-11-24 16:35:05 +0000

    Add a function to check action type of a state

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -5,6 +5,30 @@ open! Migrate_parsetree
 
 open! Ast_407
 open Parsetree
+open Efsm
+
+let state_action_type (g : G.t) (st : int) =
+  let merge_state_action_type aty1 aty2 =
+    match (aty1, aty2) with
+    | `Terminal, aty2 -> aty2
+    | aty1, `Terminal -> aty1
+    | `Send, `Send -> `Send
+    | `Recv, `Recv -> `Recv
+    | `Send, `Recv -> `Mixed
+    | `Recv, `Send -> `Mixed
+    | aty1, `Mixed -> aty1
+    | `Mixed, aty2 -> aty2
+  in
+  let f (_, a, _) acc =
+    let aty =
+      match a with
+      | SendA _ -> `Send
+      | RecvA _ -> `Recv
+      | Epsilon -> failwith "Impossible"
+    in
+    merge_state_action_type aty acc
+  in
+  G.fold_succ_e f g st `Terminal
 
 let gen_ast (_proto, _role) (_start, _g) : structure =
   let loc = Location.none in
@@ -17,7 +41,7 @@ let gen_code (proto, role) (start, g) =
   let buffer = Buffer.create 4196 in
   let formatter = Format.formatter_of_buffer buffer in
   let ast = gen_ast (proto, role) (start, g) in
-  let ast = migrate.copy_structure ast in
+  let ast = migrate.Versions.copy_structure ast in
   Pprintast.structure formatter ast ;
   Format.pp_print_flush formatter () ;
   Buffer.contents buffer
