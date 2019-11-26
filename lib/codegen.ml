b0b239c3c02035e432c53a84fce74a2b8e35e4c8
Fangyi Zhou <me@fangyi.io>
2019-11-26 23:12:55 +0000

    Generate roles

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -115,6 +115,15 @@ let find_all_payloads g =
   in
   G.fold_edges_e f g (S.singleton "string") |> S.to_list
 
+let find_all_roles g =
+  let module S = String.Set in
+  let f (_, a, _) acc =
+    match a with
+    | SendA (r, _) | RecvA (r, _) -> S.add acc r
+    | _ -> failwith "Impossible"
+  in
+  G.fold_edges_e f g S.empty |> S.to_list
+
 let gen_comms_typedef payload_types =
   let mk_recv payload_ty_str =
     let payload_ty = mk_constr payload_ty_str in
@@ -137,13 +146,25 @@ let gen_comms_typedef payload_types =
   in
   Str.type_ Asttypes.Nonrecursive [comms]
 
+let gen_role_ty roles =
+  let f role = Rtag (Location.mknoloc role, [], true, []) in
+  let role_ty = Typ.variant (List.map ~f roles) Asttypes.Closed None in
+  role_ty
+
 let gen_ast (_proto, _role) (_start, g) : structure =
   let loc = Location.none in
   let callback_typedef = gen_callback_typedef g in
   let payload_types = find_all_payloads g in
   let comms_typedef = gen_comms_typedef payload_types in
-  let vb = [%stri let hello = "hello"] in
-  [callback_typedef; comms_typedef; vb]
+  let roles = find_all_roles g in
+  let role_ty = gen_role_ty roles in
+  let run_expr = [%expr ()] in
+  let run =
+    [%stri
+      let run (router : [%t role_ty] -> comms) (callbacks : callbacks) =
+        [%e run_expr]]
+  in
+  [callback_typedef; comms_typedef; run]
 
 let gen_code (proto, role) (start, g) =
   let buffer = Buffer.create 4196 in
