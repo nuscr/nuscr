8772f245d881a84c08affdc04b7571c6e1f1d842
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2019-11-28 16:52:35 +0000

    Add mli file for codegen

---
+++ lib/codegen.mli
@@ -0,0 +1,12 @@
+(** Code generation from EFSM *)
+
+(** This module transforms a EFSM generated from local types into OCaml code. *)
+
+open Syntax
+open! Ppxlib_ast
+
+val gen_ast : name * name -> int * Efsm.G.t -> Parsetree.structure
+(** Generate AST presentation of a projected EFSM *)
+
+val gen_code : name * name -> int * Efsm.G.t -> string
+(** Generate string representation of a projected EFSM *)
