6940a65e56f7c49967e1a27db9afe3b4f66254d6
Fangyi Zhou <me@fangyi.io>
2019-11-29 00:22:27 +0000

    Resolve protocol definitions

--- lib/protocol.ml
+++ lib/protocol.ml
@@ -2,18 +2,133 @@ open! Base
 open Syntax
 open Err
 
+let rec swap_role swap_role_f {Loc.value; Loc.loc} =
+  let value =
+    match value with
+    | MessageTransfer {message; from_role; to_roles; ann} ->
+        MessageTransfer
+          { message
+          ; from_role= swap_role_f from_role
+          ; to_roles= List.map ~f:swap_role_f to_roles
+          ; ann }
+    | Recursion (rec_name, g) ->
+        Recursion (rec_name, List.map ~f:(swap_role swap_role_f) g)
+    | Continue rec_name -> Continue rec_name
+    | Choice (role, gs) ->
+        Choice
+          ( swap_role_f role
+          , List.map ~f:(List.map ~f:(swap_role swap_role_f)) gs )
+    | Do (proto, msgs, roles, ann) ->
+        Do (proto, msgs, List.map ~f:swap_role_f roles, ann)
+    | Connect (msg, r1, r2, ann) ->
+        Connect (msg, swap_role_f r1, swap_role_f r2, ann)
+    | Disconnect (r1, r2) -> Disconnect (swap_role_f r1, swap_role_f r2)
+  in
+  {value; loc}
+
+let instantiate (protocol : raw_global_protocol)
+    (replacement_roles : name list) =
+  let original_roles = protocol.roles in
+  let replacement_map = List.zip original_roles replacement_roles in
+  let replacement_map =
+    match replacement_map with
+    | List.Or_unequal_lengths.Unequal_lengths -> unimpl "Mismatched arity"
+    | List.Or_unequal_lengths.Ok replacement_map -> replacement_map
+  in
+  let replacement_map =
+    List.fold
+      ~f:(fun acc (ori, rep) ->
+        Map.add_exn acc ~key:(Name.user ori) ~data:rep)
+      ~init:(Map.empty (module String))
+      replacement_map
+  in
+  let swap_f r = Map.find_exn replacement_map (Name.user r) in
+  List.map ~f:(swap_role swap_f) protocol.interactions
+
+let rec_var_of_protocol_roles (name, roles) =
+  let names = List.map ~f:Name.user (name :: roles) in
+  Name.of_string @@ Printf.sprintf "__%s" (String.concat ~sep:"_" names)
+
 (** Unroll `do` in protocol *)
-let expand_global_protocol (protocol : global_protocol)
-    (scr_module : scr_module) =
-  let module M = Map in
+let expand_global_protocol (scr_module : scr_module)
+    (protocol : global_protocol) =
   let f acc {Loc.value= p; Loc.loc} =
-    match M.add acc ~key:(Name.user p.name) ~data:(p.interactions, loc) with
+    match
+      Map.add acc ~key:(Name.user p.name) ~data:(p, loc, List.length p.roles)
+    with
     | `Ok acc -> acc
     | `Duplicate ->
-        let _, old_loc = M.find_exn acc (Name.user p.name) in
+        let _, old_loc, _ = Map.find_exn acc (Name.user p.name) in
         uerr (RedefinedProtocol (p.name, loc, old_loc))
   in
-  let _protocols =
-    List.fold ~f ~init:(M.empty (module String)) scr_module.protocols
+  let protocols =
+    List.fold ~f ~init:(Map.empty (module String)) scr_module.protocols
   in
-  protocol
+  let known_roles = protocol.value.roles in
+  let check_role r =
+    if List.mem known_roles ~equal:Name.equal r then ()
+    else unimpl "Error message for unbound role"
+  in
+  let rec expand_aux known interactions =
+    let expand_single known ({Loc.value; Loc.loc} as i) =
+      match value with
+      | Do (name, [], roles, None) when Map.Poly.mem known (name, roles) ->
+          let known = Map.update known (name, roles) ~f:(fun _ -> true) in
+          ( known
+          , [ { Loc.value= Continue (rec_var_of_protocol_roles (name, roles))
+              ; loc } ] )
+      | Do (name, [], roles, None) ->
+          let protocol_to_expand = Map.find protocols (Name.user name) in
+          let protocol_to_expand, _, arity =
+            match protocol_to_expand with
+            | Some p -> p
+            | None -> unimpl "Error msg for unbound protocol"
+          in
+          if List.length roles <> arity then
+            unimpl "Error msg for arity mismatch" ;
+          List.iter ~f:check_role roles ;
+          let known = Map.add_exn known ~key:(name, roles) ~data:false in
+          let known, is =
+            expand_aux known (instantiate protocol_to_expand roles)
+          in
+          let has_recursion = Map.find_exn known (name, roles) in
+          let is =
+            if has_recursion then
+              [ { Loc.loc
+                ; Loc.value=
+                    Recursion (rec_var_of_protocol_roles (name, roles), is)
+                } ]
+            else is
+          in
+          let known = Map.remove known (name, roles) in
+          (known, is)
+      | Do _ -> unimpl "Do with other features"
+      | Recursion (r, is) ->
+          let known, is = expand_aux known is in
+          (known, [{i with value= Recursion (r, is)}])
+      | Choice (r, iss) ->
+          let known, iss = List.fold_map ~f:expand_aux ~init:known iss in
+          (known, [{i with value= Choice (r, iss)}])
+      | _ -> (known, [i])
+    in
+    let known, interactions =
+      List.fold_map ~f:expand_single ~init:known interactions
+    in
+    (known, List.concat interactions)
+  in
+  let top_level = (protocol.value.name, protocol.value.roles) in
+  let known, interactions =
+    expand_aux
+      (Map.Poly.singleton top_level false)
+      protocol.value.interactions
+  in
+  let has_recursion = Map.find_exn known top_level in
+  let interactions =
+    if has_recursion then
+      [ { Loc.loc= protocol.loc
+        ; Loc.value=
+            Recursion (rec_var_of_protocol_roles top_level, interactions) }
+      ]
+    else interactions
+  in
+  {protocol with Loc.value= {protocol.Loc.value with interactions}}
