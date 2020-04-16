5db6adbc1aa94dbf06a77b50ab10547202c14fb2
becharrens <bes.seb.98@gmail.com>
2020-04-16 17:38:32 +0100

    Implement function to generate unique names for local protocols

--- lib/ltype.mli
+++ lib/ltype.mli
@@ -1,5 +1,6 @@
 (** Local type management *)
 
+open! Base
 (** This module defines local types and basic operations on them. *)
 
 open Names
@@ -18,9 +19,43 @@ type t =
   | TVarL of TypeVariableName.t  (** Recursive variable *)
   | MuL of TypeVariableName.t * t  (** Fixpoint *)
   | EndL  (** Empty type *)
+  | InviteCreateL of RoleName.t list * RoleName.t list * ProtocolName.t * t
+      (** Sending invitations to existing roles and setting up dynamic
+          pariticipants *)
+  | AcceptL of
+      (* accept role' from X in Proto_role'(roles...; new roles'...) *)
+      RoleName.t
+      * RoleName.t
+      * ProtocolName.t
+      * RoleName.t list
+      * RoleName.t list
+      * t
 
 val show : t -> string
 (** Converts a local type to a string. *)
 
 val project : RoleName.t -> Gtype.t -> t
 (** Project a global type into a particular role. *)
+
+module type S = sig
+  type t [@@deriving show {with_path= false}, sexp_of]
+
+  include Comparable.S with type t := t
+end
+
+module LocalProtocolId : S
+
+(* TODO: add documentation or remove *)
+val build_local_proto_lookup_table :
+     Gtype.global_t
+  -> ( LocalProtocolId.t
+     , string
+     , LocalProtocolId.comparator_witness )
+     Base.Map.t
+
+val show_lookup_table :
+     ( LocalProtocolId.t
+     , string
+     , LocalProtocolId.comparator_witness )
+     Base.Map.t
+  -> string
