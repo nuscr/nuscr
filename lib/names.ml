3aeb0bdb0c4e5976b617465deb23b73906009148
becharrens <bes.seb.98@gmail.com>
2020-04-30 14:05:48 +0100

    Add name modules for code gen

--- lib/names.ml
+++ lib/names.ml
@@ -41,3 +41,58 @@ module TypeVariableName : TaggedName = struct
 
   let of_name (name : Name.t) = name
 end
+
+module LocalProtocolName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module ChannelStructName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module ChannelName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module InviteChannelStructName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module InviteChannelName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module CallbackName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module MessageStructName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+(* TODO: Is it needed? *)
+module ResultName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
+
+module PackageName : TaggedName = struct
+  include Name
+
+  let of_name (name : Name.t) = name
+end
