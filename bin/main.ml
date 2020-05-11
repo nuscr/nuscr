6fe5ebaa9426701d83b9a92a2ab00dabb28736f3
becharrens <bes.seb.98@gmail.com>
2020-05-11 11:46:12 +0100

    Implement codegen for messages and results using an environment

--- bin/main.ml
+++ bin/main.ml
@@ -174,15 +174,15 @@ let run_nested filename show_ast show_global show_local =
     let ltype = project_global_t g_type in
     show_result ~f:show_local_t show_local ltype ;
     ensure_unique_identifiers g_type ;
-    let protocol_msgs = gen_protocol_msgs g_type in
-    print_protocol_files protocol_msgs ;
     (* let protocol_channels = gen_protocol_channels g_type ltype in
        print_protocol_files protocol_channels ; *)
-    let {channels; invite_channels; callbacks; impl; _} =
+    let {channels; invite_channels; callbacks; impl; messages; results; _} =
       gen_code (RootDirName.of_string "Root") g_type ltype
     in
+    print_protocol_files messages ;
     print_protocol_files channels ;
     print_protocol_files invite_channels ;
+    print_protocol_files results ;
     print_local_protocol_files callbacks ;
     print_local_protocol_files impl ;
     true
