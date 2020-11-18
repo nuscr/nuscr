fcb3f95643803521f77928b4e0209590214be1bb
Fangyi Zhou <me@fangyi.io>
2020-11-18 19:15:21 +0000

    New command line interface with Cmdliner

--- bin/main.ml
+++ bin/main.ml
@@ -2,15 +2,7 @@ open Base
 open Stdio
 open Nuscrlib
 open Names
-
-let version_string () = "%%VERSION%%"
-
-let usage () =
-  "usage: "
-  ^ (Sys.get_argv ()).(0)
-  ^ " [-enum][-verbose][-fsm Role@Protocol][-project \
-     Role@Protocol][-gencode Role@Protocol][-out-dir /file/path][-go-path \
-     /file/path] file"
+open Cmdliner
 
 let parse_role_protocol_exn rp =
   match String.split rp ~on:'@' with
@@ -21,53 +13,6 @@ let parse_role_protocol_exn rp =
         (InvalidCommandLineParam
            "Role and protocol have to be for the form role@protocol")
       |> raise
-
-let enum = ref false
-
-let verbose = ref false
-
-let version = ref false
-
-let help = ref false
-
-let fsm : (RoleName.t * ProtocolName.t) option ref = ref None
-
-let project : (RoleName.t * ProtocolName.t) option ref = ref None
-
-let gencode : (RoleName.t * ProtocolName.t) option ref = ref None
-
-let out_dir : string option ref = ref None
-
-let go_path : string option ref = ref None
-
-let argspec =
-  let open Caml in
-  [ ( "-enum"
-    , Arg.Unit (fun () -> enum := true)
-    , ": list the roles and protocols in the file" )
-  ; ( "-verbose"
-    , Arg.Unit (fun () -> verbose := true)
-    , ": print extra information" )
-  ; ( "-version"
-    , Arg.Unit (fun () -> version := true)
-    , ": print the version number" )
-  ; ( "-fsm"
-    , Arg.String (fun s -> fsm := parse_role_protocol_exn s)
-    , ": project the CFSM for the specified role" )
-  ; ( "-project"
-    , Arg.String (fun s -> project := parse_role_protocol_exn s)
-    , ": project the local type for the specified role" )
-  ; ( "-gencode"
-    , Arg.String (fun s -> gencode := parse_role_protocol_exn s)
-    , ": generate code for the specified role" )
-  ; ( "-go-path"
-    , Arg.String (fun s -> go_path := Some s)
-    , ": path to the go source directory (the parent directory of the \
-       project root)" )
-  ; ( "-out-dir"
-    , Arg.String (fun s -> out_dir := Some s)
-    , ": path to the project directory inside which the code is to be \
-       generated, relative to go source directory " ) ]
 
 let process_file (fn : string) (proc : string -> In_channel.t -> 'a) : 'a =
   let input = In_channel.create fn in
@@ -80,83 +25,199 @@ let gen_output ast f = function
       print_endline res
   | _ -> ()
 
-let process_pragmas (pragmas : Syntax.pragmas) : unit =
-  let process_global_pragma (k, v) =
-    match (k, v) with
-    | Syntax.PrintUsage, _ -> usage () |> print_endline
-    | Syntax.ShowPragmas, _ -> Syntax.show_pragmas pragmas |> print_endline
-    | Syntax.NestedProtocols, _ ->
-        if Option.is_some !fsm then
-          Err.uerr (Err.IncompatibleFlag ("fsm", Syntax.show_pragma k))
+let main file enumerate verbose go_path out_dir project fsm gencode_ocaml
+    gencode_monadic_ocaml gencode_go =
+  let process_pragmas (pragmas : Syntax.pragmas) : unit =
+    let process_global_pragma (k, v) =
+      match (k, v) with
+      | Syntax.PrintUsage, _ -> ()
+      | Syntax.ShowPragmas, _ -> Syntax.show_pragmas pragmas |> print_endline
+      | Syntax.NestedProtocols, _ ->
+          if Option.is_some fsm then
+            Err.uerr (Err.IncompatibleFlag ("fsm", Syntax.show_pragma k))
+    in
+    List.iter ~f:process_global_pragma pragmas
   in
-  List.iter ~f:process_global_pragma pragmas
-
-let generate_code (ast : Syntax.scr_module) gencode =
-  let gen_ocaml role protocol =
-    Lib.generate_ocaml_code ~monad:false ast ~protocol ~role
-  in
-  let gen_go protocol =
-    match !out_dir with
-    | Some out_dir ->
-        Lib.generate_go_code ast ~protocol ~out_dir ~go_path:!go_path
-    | None ->
-        Err.UserError
-          (Err.MissingFlag
-             ( "out-dir"
-             , "This flag must be set in order to generate go implementation"
-             ))
-        |> raise
-  in
-  match gencode with
-  | Some (role, protocol) ->
-      let impl =
-        if Pragma.nested_protocol_enabled ast then gen_go protocol
-        else gen_ocaml role protocol
-      in
-      print_endline impl
-  | None -> ()
-
-let run filename verbose enumerate proj fsm gencode =
   try
-    let ast = process_file filename Lib.parse in
+    let ast = process_file file Lib.parse in
     process_pragmas ast.pragmas ;
     Lib.validate_exn ast ~verbose ;
-    if enumerate then
-      Lib.enumerate ast
-      |> List.map ~f:(fun (n, r) ->
-             RoleName.user r ^ "@" ^ ProtocolName.user n)
-      |> String.concat ~sep:"\n" |> print_endline
-    else () ;
-    gen_output ast
-      (fun ast protocol role ->
-        Lib.project_role ast ~protocol ~role |> Ltype.show)
-      proj ;
-    gen_output ast
-      (fun ast protocol role ->
-        Lib.generate_fsm ast ~protocol ~role |> snd |> Efsm.show)
-      fsm ;
-    generate_code ast gencode ;
-    true
+    let () =
+      if enumerate then
+        Lib.enumerate ast
+        |> List.map ~f:(fun (n, r) ->
+               RoleName.user r ^ "@" ^ ProtocolName.user n)
+        |> String.concat ~sep:"\n" |> print_endline
+    in
+    let () =
+      gen_output ast
+        (fun ast protocol role ->
+          Lib.project_role ast ~protocol ~role |> Ltype.show)
+        project
+    in
+    let () =
+      gen_output ast
+        (fun ast protocol role ->
+          Lib.generate_fsm ast ~protocol ~role |> snd |> Efsm.show)
+        fsm
+    in
+    let () =
+      match gencode_ocaml with
+      | Some (role, protocol) ->
+          let impl =
+            Lib.generate_ocaml_code ~monad:false ast ~protocol ~role
+          in
+          print_endline impl
+      | None -> ()
+    in
+    let () =
+      match gencode_monadic_ocaml with
+      | Some (role, protocol) ->
+          let impl =
+            Lib.generate_ocaml_code ~monad:true ast ~protocol ~role
+          in
+          print_endline impl
+      | None -> ()
+    in
+    let () =
+      match gencode_go with
+      | Some (_role, protocol) -> (
+        match out_dir with
+        | Some out_dir ->
+            let impl =
+              Lib.generate_go_code ast ~protocol ~out_dir ~go_path
+            in
+            print_endline impl
+        | None ->
+            Err.UserError
+              (Err.MissingFlag
+                 ( "out-dir"
+                 , "This flag must be set in order to generate go \
+                    implementation" ))
+            |> raise )
+      | None -> ()
+    in
+    `Ok ()
   with
   | Err.UserError msg ->
-      "User error: " ^ Err.show_user_error msg |> prerr_endline ;
-      false
-  | Err.Violation msg ->
-      "Violation: " ^ msg |> prerr_endline ;
-      false
+      `Error (false, "User error: " ^ Err.show_user_error msg)
+  | Err.Violation msg -> `Error (false, "Internal Error: " ^ msg)
   | Err.UnImplemented desc ->
-      "I'm sorry, it is unfortunate " ^ desc ^ " is not implemented"
-      |> prerr_endline ;
-      false
-  | e ->
-      "Reported problem:\n " ^ Exn.to_string e |> prerr_endline ;
-      false
+      `Error
+        ( false
+        , "I'm sorry, it is unfortunate " ^ desc ^ " is not implemented" )
+  | e -> `Error (false, "Reported problem:\n " ^ Exn.to_string e)
 
-let () =
-  let filename = ref "" in
-  Caml.Arg.parse argspec (fun fn -> filename := fn) @@ usage () ;
-  if !version then print_endline @@ version_string ()
-  else if String.length !filename = 0 then usage () |> print_endline
-  else if run !filename !verbose !enum !project !fsm !gencode then
-    Caml.exit 0
-  else Caml.exit 1
+let role_proto =
+  let parse input =
+    match String.split input ~on:'@' with
+    | [role; protocol] ->
+        Ok (RoleName.of_string role, ProtocolName.of_string protocol)
+    | _ ->
+        Error
+          (`Msg "Role and protocol have to be for the form role@protocol")
+  in
+  let print fmt (r, p) =
+    Caml.Format.pp_print_string fmt (RoleName.user r) ;
+    Caml.Format.pp_print_char fmt '@' ;
+    Caml.Format.pp_print_string fmt (ProtocolName.user p)
+  in
+  Arg.conv (parse, print)
+
+let enumerate =
+  let doc = "Enumerate the roles and protocols in the file" in
+  Arg.(value & flag & info ["enum"] ~doc)
+
+let verbose =
+  let doc = "Print extra information" in
+  Arg.(value & flag & info ["v"; "verbose"] ~doc)
+
+let go_path =
+  let doc =
+    "Path to the Go source directory (the parent directory of the project \
+     root) [Only applicable for Go Codegen]"
+  in
+  Arg.(value & opt (some dir) None & info ["go-path"] ~doc ~docv:"DIR")
+
+let project =
+  let doc =
+    "Project the local type for the specified protocol and role. \
+     <role_name>@<protocol_name>"
+  in
+  Arg.(
+    value
+    & opt (some role_proto) None
+    & info ["project"] ~doc ~docv:"ROLE@PROTO")
+
+let fsm =
+  let doc =
+    "Project the CFSM for the specified protocol and role. \
+     <role_name>@<protocol_name>"
+  in
+  Arg.(
+    value & opt (some role_proto) None & info ["fsm"] ~doc ~docv:"ROLE@PROTO")
+
+let gencode_ocaml =
+  let doc =
+    "Generate OCaml code for specified protocol and role. \
+     <role_name>@<protocol_name>"
+  in
+  Arg.(
+    value
+    & opt (some role_proto) None
+    & info ["gencode-ocaml"] ~doc ~docv:"ROLE@PROTO")
+
+let gencode_monadic_ocaml =
+  let doc =
+    "Generate monadic OCaml code for specified protocol and role. \
+     <role_name>@<protocol_name>"
+  in
+  Arg.(
+    value
+    & opt (some role_proto) None
+    & info ["gencode-ocaml-monadic"] ~doc ~docv:"ROLE@PROTO")
+
+let gencode_go =
+  let doc =
+    "Generate Go code for specified protocol and role. \
+     <role_name>@<protocol_name>"
+  in
+  Arg.(
+    value
+    & opt (some role_proto) None
+    & info ["gencode-go"] ~doc ~docv:"ROLE@PROTO")
+
+let out_dir =
+  let doc =
+    "Path to the project directory inside which the code is to be \
+     generated, relative to Go source directory [Only applicable for Go \
+     Codegen]"
+  in
+  Arg.(value & opt (some dir) None & info ["out-dir"] ~doc ~docv:"DIR")
+
+let file = Arg.(required & pos 0 (some file) None & info [] ~docv:"FILE")
+
+let cmd =
+  let doc =
+    "A tool to manipulate and validate Scribble-style multiparty protocols"
+  in
+  let man =
+    [ `S Manpage.s_description
+    ; `P
+        "$(tname) is a toolkit to manipulate Scribble-style multiparty \
+         protocols, based on classical multiparty session type theory. The \
+         toolkit provides means to define global protocols, project to \
+         local protocols, convert local protocols to a CFSM representation, \
+         and generate OCaml code for protocol implementations."
+    ; `S Manpage.s_bugs
+    ; `P "Please report bugs on GitHub at %%PKG_ISSUES%%" ]
+  in
+  ( Term.(
+      ret
+        ( const main $ file $ enumerate $ verbose $ go_path $ out_dir
+        $ project $ fsm $ gencode_ocaml $ gencode_monadic_ocaml $ gencode_go
+        ))
+  , Term.info "nuscr" ~version:"%%VERSION%%" ~doc ~exits:Term.default_exits
+      ~man )
+
+let () = Term.(exit @@ eval cmd)
