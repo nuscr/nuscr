eb592f881282272f43f7d18866c214f8a6f26f2a
Fangyi Zhou <me@fangyi.io>
2020-12-10 15:38:10 +0000

    Put refinement types under a pragma

--- lib/pragma.mli
+++ lib/pragma.mli
@@ -3,3 +3,6 @@ val parse_pragmas_string : string -> Syntax.pragmas
 
 val nested_protocol_enabled : Syntax.scr_module -> bool
 (** Whether nested protocol pragma is enabled for a given module *)
+
+val refinement_types_enabled : Syntax.scr_module -> bool
+(** Whether refinement types pragma is enabled for a given module *)
