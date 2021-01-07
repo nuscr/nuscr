997c51fe5d981e46aa220af57100c08247d1f4c7
Fangyi Zhou <me@fangyi.io>
2021-01-07 17:56:47 +0000

    Implement interaction with solvers

--- lib/solver.ml
+++ lib/solver.ml
@@ -1,1 +1,7 @@
-let run_solver _input = failwith "TODO"
+open! Base
+
+let run_solver input =
+  let input = Bytes.of_string input in
+  let output = Process.read_stdout ~stdin:input "z3" [|"-in"|] in
+  List.iter ~f:Stdio.print_endline output ;
+  String.concat ~sep:"\n" output
