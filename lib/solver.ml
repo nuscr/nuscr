3907d34aaae8822a19b9f9527d46979048b9cce0
Fangyi Zhou <me@fangyi.io>
2021-01-08 20:28:03 +0000

    Remove a debug print

--- lib/solver.ml
+++ lib/solver.ml
@@ -3,5 +3,4 @@ open! Base
 let run_solver input =
   let input = Bytes.of_string input in
   let output = Process.read_stdout ~stdin:input "z3" [|"-in"|] in
-  List.iter ~f:Stdio.print_endline output ;
   String.concat ~sep:"\n" output
