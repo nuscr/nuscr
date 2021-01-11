eb3ca7f3cae7cc5fa4ae4eeff5598fc97660ba21
Fangyi Zhou <me@fangyi.io>
2021-01-11 14:47:58 +0000

    Fix z3 issue

--- lib/solver.ml
+++ lib/solver.ml
@@ -2,5 +2,7 @@ open! Base
 
 let run_solver input =
   let input = Bytes.of_string input in
-  let output = Process.read_stdout ~stdin:input "z3" [|"-in"|] in
+  (* https://stackoverflow.com/questions/30998111/executing-a-z3-script-in-command-line-prompt
+   * *)
+  let output = Process.read_stdout ~stdin:input "z3" [|"-smt2"; "-in"|] in
   String.concat ~sep:"\n" output
