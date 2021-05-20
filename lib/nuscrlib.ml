4df8801567bc51580c41397a90f55d954b0420a4
Fangyi Zhou <me@fangyi.io>
2021-05-20 21:19:16 +0800

    Manually select modules to export

---
+++ lib/nuscrlib.ml
@@ -0,0 +1,21 @@
+include Lib
+
+module Pragma = struct
+  include Pragma
+end
+
+module Gtype = struct
+  include Gtype
+end
+
+module Ltype = struct
+  include Ltype
+end
+
+module Efsm = struct
+  include Efsm
+end
+
+module Err = struct
+  include Err
+end
--- test/test.ml
+++ test/test.ml
@@ -77,9 +77,9 @@ let process_files fns =
           in
           try
             In_channel.seek in_channel 0L ;
-            let ast = Nuscrlib.Lib.parse fn in_channel in
+            let ast = Nuscrlib.parse fn in_channel in
             process_pragmas ast.pragmas ;
-            Nuscrlib.Lib.validate_exn ast ;
+            Nuscrlib.validate_exn ast ;
             if is_negative_test then raise ExpectFail
           with
           | Nuscrlib.Err.UnImplemented _ -> ()
