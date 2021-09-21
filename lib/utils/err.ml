46920e15a65a473ea0248160309d039dc302aef8
Fangyi Zhou <me@fangyi.io>
2021-09-21 18:20:36 +0100

    Use sprintf for formatting errors

--- lib/utils/err.ml
+++ lib/utils/err.ml
@@ -1,5 +1,6 @@
 open! Base
 open Names
+open Printf
 
 type user_error =
   | UnknownPragma of string
@@ -42,84 +43,85 @@ exception UserError of user_error
 let show_user_error = function
   | UnknownPragma prg -> "Unknown pragma: " ^ prg
   | IncompatibleFlag (flag, pragma) ->
-      "Incompatible flag: " ^ flag ^ " set with pragma: " ^ pragma
-  | MissingFlag (flag, msg) -> "Flag: " ^ flag ^ " is not set. " ^ msg
-  | PragmaNotSet (prg, msg) -> "Pramga: " ^ prg ^ " is not set. " ^ msg
+      sprintf "Incompatible flag: %s set with pragma: %s" flag pragma
+  | MissingFlag (flag, msg) -> sprintf "Flag: %s is not set. %s" flag msg
+  | PragmaNotSet (prg, msg) -> sprintf "Pramga: %s is not set. %s" prg msg
   | LexerError msg -> "Lexer error: " ^ msg
   | ParserError interval ->
       "Parser error: An error occurred at " ^ Loc.show interval
   | UnboundRecursionName n ->
-      "Unbound name " ^ TypeVariableName.user n ^ " in `continue` at "
-      ^ Loc.show (TypeVariableName.where n)
-  | RedefinedRecursionName (name, interval1, interval2) ->
-      "Redefined name "
-      ^ TypeVariableName.user name
-      ^ " of `rec` at " ^ Loc.show interval1 ^ " and " ^ Loc.show interval2
+      sprintf "Unbound name %s in `continue` at %s" (TypeVariableName.user n)
+        (Loc.show (TypeVariableName.where n))
+  | RedefinedRecursionName (name, loc1, loc2) ->
+      sprintf "Redefined name %s of `rec` at %s and %s"
+        (TypeVariableName.user name)
+        (Loc.show loc1) (Loc.show loc2)
   | Uncategorised msg -> "Error " ^ msg
   | InvalidCommandLineParam msg -> "Invalid command line parameter: " ^ msg
   | UnboundRole r ->
-      "Unbound role " ^ RoleName.user r ^ " at " ^ Loc.show
-      @@ RoleName.where r
+      sprintf "Unbound role %s at %s" (RoleName.user r)
+        (Loc.show (RoleName.where r))
   | ReflexiveMessage (r, loc1, loc2) ->
       let loc_merge = Loc.merge loc1 loc2 in
-      "Reflexive message of Role " ^ RoleName.user r ^ " at "
-      ^ Loc.show loc_merge
+      sprintf "Reflexive message of role %s at %s" (RoleName.user r)
+        (Loc.show loc_merge)
   | UnableToMerge s -> "Unable to merge: " ^ s
-  | RedefinedProtocol (name, interval1, interval2) ->
-      "Redefined protocol " ^ ProtocolName.user name ^ " at "
-      ^ Loc.show interval1 ^ " and " ^ Loc.show interval2
+  | RedefinedProtocol (name, loc1, loc2) ->
+      sprintf "Redefined protocol %s at %s and %s" (ProtocolName.user name)
+        (Loc.show loc1) (Loc.show loc2)
   | UnboundProtocol p ->
-      "Unbound protocol call " ^ ProtocolName.user p ^ " at " ^ Loc.show
-      @@ ProtocolName.where p
+      sprintf "Unbound protocol call %s at %s" (ProtocolName.user p)
+        (Loc.show (ProtocolName.where p))
   | ArityMismatch (p, expected, actual) ->
-      "Protocol arity mismatch, " ^ ProtocolName.user p ^ " requires "
-      ^ Int.to_string expected ^ " roles, but " ^ Int.to_string actual
-      ^ " is given"
+      sprintf
+        "Protocol arity mismatch, %s requires %d roles, but %d is given"
+        (ProtocolName.user p) expected actual
   | InconsistentNestedChoice (r1, r2) ->
-      "Inconsistent nested choice, a choice at " ^ RoleName.user r1 ^ " at "
-      ^ Loc.show (RoleName.where r1)
-      ^ " cannot be followed with a choice at " ^ RoleName.user r2 ^ " at "
-      ^ Loc.show (RoleName.where r2)
+      sprintf
+        "Inconsistent nested choice, a choice at %s at %s cannot be \
+         followed by a choice at %s at %s"
+        (RoleName.user r1)
+        (Loc.show (RoleName.where r1))
+        (RoleName.user r2)
+        (Loc.show (RoleName.where r2))
   | RoleMismatch (expected, actual) ->
-      "Expecting role " ^ RoleName.user expected ^ ", but got "
-      ^ RoleName.user actual ^ " at "
-      ^ Loc.show (RoleName.where actual)
+      sprintf "Expecting role %s, but got %s at %s" (RoleName.user expected)
+        (RoleName.user actual)
+        (Loc.show (RoleName.where actual))
   | DuplicateLabel l ->
-      "Duplicate label " ^ LabelName.user l ^ " in choices at "
-      ^ Loc.show (LabelName.where l)
+      sprintf "Duplicate label %s in choices at %s" (LabelName.user l)
+        (Loc.show (LabelName.where l))
   | DuplicateRoleArgs called_proto ->
-      "Duplicate role arguments in call to protocol "
-      ^ ProtocolName.user called_proto
-      ^ " at "
-      ^ Loc.show (ProtocolName.where called_proto)
+      sprintf "Duplicate role arguments in call to protocol %s at %s"
+        (ProtocolName.user called_proto)
+        (Loc.show (ProtocolName.where called_proto))
   | DuplicateRoleParams protocol ->
-      "Duplicate role parameter in declaration of protocol "
-      ^ ProtocolName.user protocol
-      ^ " at "
-      ^ Loc.show (ProtocolName.where protocol)
+      sprintf "Duplicate role parameter in declaration of protocol %s at %s"
+        (ProtocolName.user protocol)
+        (Loc.show (ProtocolName.where protocol))
   | ChoiceCallRoleMismatch called_proto ->
-      "Invalid call to protocol '"
-      ^ ProtocolName.user called_proto
-      ^ "' in choice at "
-      ^ Loc.show (ProtocolName.where called_proto)
-      ^ "\n\
+      sprintf
+        "Invalid call to protocol '%s' in choice at %s\n\
          Some role participating in call must receive first message in all \
          branches"
+        (ProtocolName.user called_proto)
+        (Loc.show (ProtocolName.where called_proto))
   | DuplicatePayloadField (label, field) ->
-      "Duplicate field name '" ^ VariableName.user field ^ "' in message '"
-      ^ LabelName.user label ^ "' at " ^ Loc.show @@ LabelName.where label
+      sprintf "Duplicate field name '%s' in message '%s' at %s"
+        (VariableName.user field) (LabelName.user label)
+        (Loc.show (LabelName.where label))
   | FileSysErr msg -> "File System Error: " ^ msg
   | ProtocolNotFound p -> "Cannot find protocol: " ^ ProtocolName.user p
   | IllFormedPayloadType ty -> "Ill-formed payload type: " ^ ty
   | TypeError (expr, ty) ->
-      Printf.sprintf "Type Error: Expression %s should be of type %s" expr ty
+      sprintf "Type Error: Expression %s should be of type %s" expr ty
   | UnknownVariableValue (role, var) ->
-      Printf.sprintf "Role %s does not know the value of the variable %s"
+      sprintf "Role %s does not know the value of the variable %s"
         (RoleName.user role) (VariableName.user var)
   | UnsatisfiableRefinement -> "Refinements cannot be satisfied"
   | StuckRefinement -> "Protocol may be stuck due to refinements"
   | UnguardedTypeVariable tv ->
-      Printf.sprintf "Unguarded type variable %s at %s"
+      sprintf "Unguarded type variable %s at %s"
         (TypeVariableName.user tv)
         (Loc.show (TypeVariableName.where tv))
 
