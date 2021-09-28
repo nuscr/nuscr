1eaeba98ca2ca9a1b6b4d3fde88a473da5bfd306
Fangyi Zhou <me@fangyi.io>
2021-09-28 18:21:30 +0100

    Unexport unused definitions in Gocodegen

--- lib/codegen/go/gocodegen.mli
+++ lib/codegen/go/gocodegen.mli
@@ -1,38 +1,6 @@
 open! Base
 open Gonames
 
-(** Record containing the implementation of all the components of the Go
-    protocol implementation *)
-type codegen_result =
-  { messages: string
-  ; channels: string Map.M(ProtocolName).t
-  ; invite_channels: string Map.M(ProtocolName).t
-  ; results: string Map.M(ProtocolName).t
-  ; impl: string Map.M(LocalProtocolName).t
-  ; callbacks: string Map.M(LocalProtocolName).t
-  ; protocol_setup: string Map.M(ProtocolName).t
-  ; entry_point: string }
-
-val show_codegen_result :
-  codegen_result -> ProtocolName.t -> RootDirName.t -> string
-(** Convert the code generation result to a string, showing the contents of
-    each generated implementation file *)
-
-val gen_code :
-     RootDirName.t
-  -> ProtocolName.t
-  -> Gtype.nested_t
-  -> Ltype.nested_t
-  -> codegen_result
-(** Function which generates the Go implementation for the entry-point
-    protocol and all the protocols in the Scribble module*)
-
-val ensure_unique_identifiers : Gtype.nested_t -> unit
-(** Function which verifies that the Scribble module satisfies the
-    constraints needed in order to be able to generate implementation. Some
-    of these constraints may be too restrictive right now, but they should be
-    easy to relax *)
-
 val generate_go_code :
      Syntax.scr_module
   -> protocol:ProtocolName.t
