ba33b40420705ca4fd27ccc88721be17a415454e
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-05-16 15:51:15 +0100

    Document LiteratureSyntax

--- lib/mpst/literatureSyntax.mli
+++ lib/mpst/literatureSyntax.mli
@@ -1,28 +1,42 @@
+(** A Multiparty Session Type representation that is similar to those used in
+    the literature *)
+
 open! Base
 open Names
 
 type 'a cont_list = (LabelName.t * PayloadTypeName.t list * 'a) list
 
+(** Global Types with basic features, in a form that is similar to those seen
+    in standard literature. (No choice constructor, all choices are directed) *)
 type global =
   | BranchG of
       { g_br_from: RoleName.t
       ; g_br_to: RoleName.t
       ; g_br_cont: global cont_list }
-  | MuG of TypeVariableName.t * global
-  | TVarG of TypeVariableName.t
-  | EndG
+      (** p -> q : \{ l_i(S_i) . G_i \}_\{i \in I\} *)
+  | MuG of TypeVariableName.t * global  (** mu t. G *)
+  | TVarG of TypeVariableName.t  (** t *)
+  | EndG  (** end *)
 
 type local =
   | SendL of RoleName.t * local cont_list
+      (** !p \{ l_i(S_i) . L_i \}_\{i \in I\} *)
   | RecvL of RoleName.t * local cont_list
-  | MuL of TypeVariableName.t * local
-  | TVarL of TypeVariableName.t
-  | EndL
+      (** ?p \{ l_i(S_i) . L_i \}_\{i \in I\} *)
+  | MuL of TypeVariableName.t * local  (** mu t. L *)
+  | TVarL of TypeVariableName.t  (** t *)
+  | EndL  (** end *)
 
 val from_gtype : Gtype.t -> global
+(** Convert from a {!Gtype.t} *)
 
 val from_ltype : Ltype.t -> local
+(** Convert from a {!Ltype.t} *)
 
 val show_gtype_mpstk : global -> string
+(** Output the global type in a form recognised by MPSTK.
+    https://github.com/alcestes/mpstk *)
 
 val show_ltype_mpstk : local -> string
+(** Output the local type in a form recognised by MPSTK.
+    https://github.com/alcestes/mpstk *)
