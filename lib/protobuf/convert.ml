de27f8101676512c63897842d59f7b3ebc6f9d50
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-08-08 14:48:58 +0100

    protobuf: define a tentative form of local types

--- lib/protobuf/convert.ml
+++ lib/protobuf/convert.ml
@@ -2,6 +2,7 @@ open! Base
 open Names
 open LiteratureSyntax
 open Globaltype
+open Localtype
 
 let of_gtype gtype =
   let gtype = LiteratureSyntax.from_gtype gtype in
@@ -18,9 +19,11 @@ let of_gtype gtype =
             let add_cont (idx, pb_gtype) (label, _, cont) =
               let label = LabelName.user label in
               let action_send =
-                Action.make ~idx ~type':Action.ActionType.SEND ~from_role
+                GlobalAction.make ~idx
+                  ~type':GlobalAction.GlobalActionType.SEND ~from_role
                   ~to_role
-                  ~continuations:[Action.Index.make ~next:(idx + 1) ~label ()]
+                  ~continuations:
+                    [GlobalAction.Index.make ~next:(idx + 1) ~label ()]
                   ()
               in
               let pb_gtype =
@@ -36,9 +39,11 @@ let of_gtype gtype =
                 | _ -> idx + 1
               in
               let action_recv =
-                Action.make ~idx ~type':Action.ActionType.RECV ~from_role
+                GlobalAction.make ~idx
+                  ~type':GlobalAction.GlobalActionType.RECV ~from_role
                   ~to_role
-                  ~continuations:[Action.Index.make ~next:next_idx ~label ()]
+                  ~continuations:
+                    [GlobalAction.Index.make ~next:next_idx ~label ()]
                   ()
               in
               let idx = idx + 1 in
@@ -61,11 +66,64 @@ let of_gtype gtype =
             in
             aux tyvars idx pb_gtype (g :: rest)
         | TVarG _ :: _ ->
-            Err.violationf ~here:[%here] "TVarG should not appear here"
+            Err.violation ~here:[%here] "TVarG should not appear here"
         | EndG :: _ ->
-            Err.violationf ~here:[%here] "TVarG should not appear here"
+            Err.violation ~here:[%here] "EndG should not appear here"
       in
       let _, output =
         aux (Map.empty (module TypeVariableName)) 0 protobuf_gtype [gtype]
+      in
+      output
+
+let of_ltype ltype =
+  let ltype = LiteratureSyntax.from_ltype ltype in
+  match ltype with
+  | EndL -> LocalType.make ~start:(-1) ()
+  | ltype ->
+      let protobuf_ltype = LocalType.make () in
+      let rec aux tyvars idx pb_ltype = function
+        | [] -> (idx, pb_ltype)
+        | (SendL (partner_role, conts) as ltype) :: rest
+         |(RecvL (partner_role, conts) as ltype) :: rest ->
+            let partner_role = RoleName.user partner_role in
+            let add_cont (idx, pb_ltype) (label, _, cont) =
+              let label = LabelName.user label in
+              let idx = idx + 1 in
+              let action_type =
+                match ltype with
+                | SendL _ -> LocalAction.LocalActionType.SEND
+                | RecvL _ -> LocalAction.LocalActionType.RECV
+                | _ -> Err.violation ~here:[%here] "Expect SendL or RecvL"
+              in
+              let action =
+                LocalAction.make ~idx ~type':action_type ~partner_role
+                  ~continuations:
+                    [LocalAction.Index.make ~next:(idx + 1) ~label ()]
+                  ()
+              in
+              let pb_ltype =
+                { pb_ltype with
+                  LocalType.actions= action :: pb_ltype.LocalType.actions }
+              in
+              match cont with
+              | EndL | TVarL _ -> aux tyvars idx pb_ltype rest
+              | cont -> aux tyvars idx pb_ltype (cont :: rest)
+            in
+            List.fold ~f:add_cont ~init:(idx, pb_ltype) conts
+        | MuL (tvar, g) :: rest ->
+            let tyvars =
+              match Map.add tyvars ~key:tvar ~data:idx with
+              | `Ok tyvars -> tyvars
+              | `Duplicate ->
+                  Err.unimpl ~here:[%here] "alpha-renaming for tyvars"
+            in
+            aux tyvars idx pb_ltype (g :: rest)
+        | TVarL _ :: _ ->
+            Err.violation ~here:[%here] "TVarL should not appear here"
+        | EndL :: _ ->
+            Err.violation ~here:[%here] "EndL should not appear here"
+      in
+      let _, output =
+        aux (Map.empty (module TypeVariableName)) 0 protobuf_ltype [ltype]
       in
       output
--- lib/protobuf/convert.mli
+++ lib/protobuf/convert.mli
@@ -1,1 +1,3 @@
 val of_gtype : Gtype.t -> Globaltype.GlobalType.t
+
+val of_ltype : Ltype.t -> Localtype.LocalType.t
